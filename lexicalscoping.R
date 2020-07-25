888c1615c50de4ea569fae32a4e32d812e1ea227
sannidhi353 <68779176+sannidhi353@users.noreply.github.com>
2020-07-25 18:41:52 +0530

    Add files via upload

---
+++ lexicalscoping.R
@@ -0,0 +1,31 @@
+##
+##I Simply set the input x as a matrix
+##and then set the solved value "s" as null
+##then I changed  every reference to "mean" and "solve"
+makeCacheMatrix<- function (x= matrix(sample(1:100,9),3,3)) {
+      s<-NULL
+	set<-function(y){
+	x<<-y
+	s<<-NULL
+      }
+	get<-function() x
+	setsolve<-function(solve) s<<- solve
+	getsolve<- function() s
+	list(set=set, get=get,
+	 setsolve=setsolve, 
+	getsolve=getsolve)
+	}
+##
+##Same here,changed "mean" to "solve" and "m" to "n"
+cacheSolve<-function(x, ...){
+	s<-x$getsolve()
+	if(!is.null(s)) {
+	 message("getting inversed matrix")
+	return(s)
+	}
+	data<- x$get()
+	s<- solve(data,...)
+	x$setsolve(s)
+	s
+}
+ 
